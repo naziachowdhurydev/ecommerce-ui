88a1f8021b7404fdaa0ccc00442c2e37f0595fa7
naziachowdhurydev <naziachowdhurydev.email.com>
2025-05-03 20:06:07 +0600

    featured post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <>
       {/* header */}
-      <header className="max-w-[1400px] h-[136px] mx-auto flex justify-between ">
-        <div className="flex gap-13.5">
-          <span className="font-bold text-[28px]">BrandName</span>
+      <header className="w-[1400px] h-[136px] mx-auto flex justify-between">
+        <div className="flex gap-13.5 items-center">
+          <span className="font-primary font-bold text-[28px]">BrandName</span>
           <nav className="text-[18px] p-2">
             <ul className="flex gap-8 text-1rem text-[#727272]">
               <li>
@@ -40,7 +40,7 @@ export default function Home() {
             </ul>
           </nav>
         </div>
-        <div className="py-2.5">
+        <div className="py-2.5 flex">
           <div className="flex gap-4 items-center ">
             <svg
               width="16"
@@ -128,10 +128,10 @@ export default function Home() {
       </header>
       {/* editors pick */}
       <div className="bg-[#FAFAFA] py-20">
-        <div className="max-w-[1200px] mx-auto space-y-12">
+        <div className="max-w-[1050px] mx-auto space-y-12">
           <div className="space-y-2.5">
             <h2 className="font-primary mb-2.5 font-bold text-2xl leading-8 tracking-[0.1px] text-center text-[#252B42]">
-              EDITOR`S PICK
+              EDITOR'S PICK
             </h2>
             <p className="font-primary font-normal text-sm leading-6 tracking-[0.2px] text-center text-[#737373]">
               Problems trying to resolve the conflict between
@@ -191,19 +191,19 @@ export default function Home() {
         </div>
       </div>
       {/*product card */}
-      <div className="max-w-[1050px]  mx-auto">
+      <div className="max-w-[1050px] mx-auto">
         <div className="text-center font-primary py-20 ">
           <h4 className="text-[#727272] text-sm leading-5 tracking-[0.2px] font-normal">
             Featured Products
           </h4>
-          <h3 className="text-2xl leading-8 tracking-[0.2px] font-bold text-[#252B42]">
+          <h3 className="text-2xl font-primary leading-8 tracking-[0.2px] font-bold text-[#252B42]">
             BESTSELLER PRODUCTS
           </h3>
-          <h4 className="text-sm font-bold leading-5 tracking-[0.2px] text-[#737373]">
+          <h4 className="text-sm font-primary font-bold leading-5 tracking-[0.2px] text-[#737373]">
             Problems trying to resolve the confilct between
           </h4>
         </div>
-        <div className="grid grid-cols-4 gap-8 max-w-[1049px] mx-auto">
+        <div className="grid grid-cols-4 gap-8 max-w-[1050px] font-primary mx-auto">
           <div className="w-[239px] h-[615px]">
             <img
               className=" w[239px] h-[427px]"
@@ -211,13 +211,13 @@ export default function Home() {
               alt=""
             />
             <div className="w-[239px] h-[188px]">
-              <h3 className="text-[#252B42] text-[16px] pt-6 font-bold leading-6 tracking-[o.2px] text-center">
+              <h3 className="text-[#252B42] text-[16px] font-primary pt-6 font-bold leading-6 tracking-[o.2px] text-center">
                 Graphic Design
               </h3>
-              <h4 className="text-[14px] leading-6 tracking-[0.2px] py-2.5 px-3.5 text-[#737373] font-bold text-center">
+              <h4 className="text-[14px] font-primary leading-6 tracking-[0.2px] py-2.5 px-3.5 text-[#737373] font-bold text-center">
                 English Department
               </h4>
-              <h4 className="text-[16px] leading-6 tracking-[0.2px] text-center font-bold text-[#BDBDBD]">
+              <h4 className="text-[16px] font-primary leading-6 tracking-[0.2px] text-center font-bold text-[#BDBDBD]">
                 $16.48
                 <span className="text-[16px] ml-0.5 font-bold text-[#23856D]">
                   $6.48
@@ -374,9 +374,156 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      {/* featured post */}
+      <div className="max-w-[1050px] mx-auto py-28">
+        <div className=" mx-auto text-center pb-20">
+          <h6 className="text-primary text-sm leading-6 tracking-[0.2px] font-bold">
+            Practice Advice
+          </h6>
+          <h2 className="font-primary text-[40px] py-2.5leading-12 tracking-[0.2px] font-bold">
+            Featured Posts
+          </h2>
+          <p className="font-primary font-normal text-[#737373] text-sm leading-5 tracking-[0.2px]">
+            Problems trying to resolve the conflict between <br /> the two major
+            realms of Classical physics: Newtonian mechanics
+          </p>
+        </div>
+        <div className="flex gap-8 ">
+          <div className="w-[328px] h-[606px] border-[1px] bg-[#FFFFFF] shadow-md border-[#f5f5f5] border-solid">
+            <img
+              className="w-full h-[300px]"
+              src="/images/featured/featured-pic-1.jpg"
+              alt=""
+            />
+
+            <div className="p-6">
+              <div className="flex w-159px ">
+                <span className="font-primary text-[#8EC2F2] text-[12px] leading-4 tracking-[0.2px] font-normal">
+                  Google
+                </span>
+                <span className="font-primary text-[#737373] px-3 text-[12px] leading-4 tracking-[0.2px] font-normal">
+                  Trending
+                </span>
+                <span className="font-primary text-[#737373] text-[12px] leading-4 tracking-[0.2px] font-normal">
+                  New
+                </span>
+              </div>
+              <h5 className="font-primary text-[20px] py-2.5 leading-8 tracking-[0.2px] font-normal text-[#252B42]">
+                Loudest à la Madison #1 (L'integral)
+              </h5>
+              <p className="font-primary text-sm text-[#737373] leading-5 tracking-[0.2px] font-normal">
+                We focus on ergonomics and meeting you where you work. It's only
+                a keystroke away.
+              </p>
+              <div className="flex justify-between py-2.5">
+                <span className="font-primary text-[12px] text-[#737373] leading-4 tracking-[0.2px]">
+                  22 April 2021
+                </span>
+                <span className="font-primary text-[12px] text-[#737373] leading-4 tracking-[0.2px]">
+                  10 Comments
+                </span>
+              </div>
+              <a
+                className="font-primary text-sm text-[#737373] font-bold leading-6 tracking-[0.2px]"
+                href=""
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
+
+          <div className="w-[328px] h-[606px] border-[1px] bg-[#FFFFFF] shadow-md border-[#f5f5f5] border-solid">
+            <img
+              className="w-full h-[300px]"
+              src="/images/featured/featured-pic-2.jpg"
+              alt=""
+            />
+
+            <div className="p-6">
+              <div className="flex w-159px ">
+                <span className="font-primary text-[#8EC2F2] text-[12px] leading-4 tracking-[0.2px] font-normal">
+                  Google
+                </span>
+                <span className="font-primary text-[#737373] px-3 text-[12px] leading-4 tracking-[0.2px] font-normal">
+                  Trending
+                </span>
+                <span className="font-primary text-[#737373] text-[12px] leading-4 tracking-[0.2px] font-normal">
+                  New
+                </span>
+              </div>
+              <h5 className="font-primary text-[20px] py-2.5 leading-8 tracking-[0.2px] font-normal text-[#252B42]">
+                Loudest à la Madison #1 (L'integral)
+              </h5>
+              <p className="font-primary text-sm text-[#737373] leading-5 tracking-[0.2px] font-normal">
+                We focus on ergonomics and meeting you where you work. It's only
+                a keystroke away.
+              </p>
+              <div className="flex justify-between py-2.5">
+                <span className="font-primary text-[12px] text-[#737373] leading-4 tracking-[0.2px]">
+                  22 April 2021
+                </span>
+                <span className="font-primary text-[12px] text-[#737373] leading-4 tracking-[0.2px]">
+                  10 Comments
+                </span>
+              </div>
+              <a
+                className="font-primary text-sm text-[#737373] font-bold leading-6 tracking-[0.2px]"
+                href=""
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
+
+          <div className="w-[328px] h-[606px] border-[1px] bg-[#FFFFFF] shadow-md border-[#f5f5f5] border-solid">
+            <img
+              className="w-full h-[300px]"
+              src="/images/featured/featured-pic-3.jpg"
+              alt=""
+            />
+
+            <div className="p-6">
+              <div className="flex w-159px ">
+                <span className="font-primary text-[#8EC2F2] text-[12px] leading-4 tracking-[0.2px] font-normal">
+                  Google
+                </span>
+                <span className="font-primary text-[#737373] px-3 text-[12px] leading-4 tracking-[0.2px] font-normal">
+                  Trending
+                </span>
+                <span className="font-primary text-[#737373] text-[12px] leading-4 tracking-[0.2px] font-normal">
+                  New
+                </span>
+              </div>
+              <h5 className="font-primary text-[20px] py-2.5 leading-8 tracking-[0.2px] font-normal text-[#252B42]">
+                Loudest à la Madison #1 (L'integral)
+              </h5>
+              <p className="font-primary text-sm text-[#737373] leading-5 tracking-[0.2px] font-normal">
+                We focus on ergonomics and meeting you where you work. It's only
+                a keystroke away.
+              </p>
+              <div className="flex justify-between py-2.5">
+                <span className="font-primary text-[12px] text-[#737373] leading-4 tracking-[0.2px]">
+                  22 April 2021
+                </span>
+                <span className="font-primary text-[12px] text-[#737373] leading-4 tracking-[0.2px]">
+                  10 Comments
+                </span>
+              </div>
+              <a
+                className="font-primary text-sm text-[#737373] font-bold leading-6 tracking-[0.2px]"
+                href=""
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* footer */}
       <footer>
-        <div className=" bg-[#FAFAFA]  py-10">
+        <div className=" bg-[#FAFAFA] font-primary py-10">
           <div className=" max-w-[1050px]  mx-auto flex justify-between items-center">
             <h3 className="text-[30px] font-bold">Bandage</h3>
             <div className="flex gap-4 items-center">
@@ -402,7 +549,7 @@ export default function Home() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <g clip-path="url(#clip0_2984_7848)">
+                <g clipPath="url(#clip0_2984_7848)">
                   <path
                     d="M13 1C10.0149 1 9.6395 1.01375 8.46663 1.066C7.29375 1.121 6.49487 1.30525 5.795 1.5775C5.06088 1.85363 4.39593 2.28676 3.84663 2.84663C3.2871 3.39621 2.85402 4.06108 2.5775 4.795C2.30525 5.4935 2.11963 6.29375 2.066 7.4625C2.01375 8.63812 2 9.01213 2 12.0014C2 14.9879 2.01375 15.3619 2.066 16.5347C2.121 17.7063 2.30525 18.5051 2.5775 19.205C2.85938 19.9282 3.23475 20.5415 3.84663 21.1534C4.45713 21.7652 5.07037 22.142 5.79363 22.4225C6.49487 22.6947 7.29238 22.8804 8.46388 22.934C9.63813 22.9862 10.0121 23 13 23C15.9879 23 16.3605 22.9862 17.5347 22.934C18.7049 22.879 19.5065 22.6947 20.2064 22.4225C20.94 22.1462 21.6045 21.7131 22.1534 21.1534C22.7652 20.5415 23.1406 19.9282 23.4225 19.205C23.6934 18.5051 23.879 17.7063 23.934 16.5347C23.9862 15.3619 24 14.9879 24 12C24 9.01213 23.9862 8.63813 23.934 7.46388C23.879 6.29375 23.6934 5.4935 23.4225 4.795C23.146 4.06106 22.7129 3.39618 22.1534 2.84663C21.6042 2.28655 20.9392 1.85339 20.205 1.5775C19.5037 1.30525 18.7035 1.11963 17.5334 1.066C16.3591 1.01375 15.9865 1 12.9973 1H13.0014H13ZM12.0141 2.98275H13.0014C15.9384 2.98275 16.2863 2.99238 17.4454 3.046C18.5179 3.09413 19.1009 3.27425 19.4886 3.42412C20.0015 3.6235 20.3686 3.86275 20.7536 4.24775C21.1386 4.63275 21.3765 4.9985 21.5759 5.51275C21.7271 5.89913 21.9059 6.48213 21.954 7.55463C22.0076 8.71375 22.0186 9.06163 22.0186 11.9973C22.0186 14.9329 22.0076 15.2821 21.954 16.4412C21.9059 17.5138 21.7257 18.0954 21.5759 18.4831C21.3995 18.9607 21.118 19.3926 20.7522 19.7467C20.3672 20.1318 20.0015 20.3696 19.4873 20.569C19.1023 20.7203 18.5192 20.899 17.4454 20.9485C16.2863 21.0007 15.9384 21.0131 13.0014 21.0131C10.0644 21.0131 9.71513 21.0007 8.556 20.9485C7.4835 20.899 6.90188 20.7203 6.51412 20.569C6.03631 20.3929 5.60405 20.1119 5.24913 19.7467C4.88303 19.392 4.60112 18.9598 4.42412 18.4818C4.27425 18.0954 4.09413 17.5124 4.046 16.4399C3.99375 15.2808 3.98275 14.9329 3.98275 11.9945C3.98275 9.0575 3.99375 8.711 4.046 7.55187C4.0955 6.47937 4.27425 5.89638 4.4255 5.50863C4.62488 4.99575 4.86412 4.62862 5.24913 4.24362C5.63412 3.85862 5.99988 3.62075 6.51412 3.42138C6.90188 3.27013 7.4835 3.09138 8.556 3.04188C9.57075 2.99513 9.964 2.98138 12.0141 2.98V2.98275ZM18.8726 4.80875C18.6993 4.80875 18.5276 4.84289 18.3675 4.90923C18.2073 4.97557 18.0618 5.0728 17.9392 5.19537C17.8167 5.31794 17.7194 5.46346 17.6531 5.62361C17.5868 5.78376 17.5526 5.95541 17.5526 6.12875C17.5526 6.30209 17.5868 6.47374 17.6531 6.63389C17.7194 6.79404 17.8167 6.93956 17.9392 7.06213C18.0618 7.1847 18.2073 7.28193 18.3675 7.34827C18.5276 7.41461 18.6993 7.44875 18.8726 7.44875C19.2227 7.44875 19.5585 7.30968 19.806 7.06213C20.0536 6.81458 20.1926 6.47884 20.1926 6.12875C20.1926 5.77866 20.0536 5.44292 19.806 5.19537C19.5585 4.94782 19.2227 4.80875 18.8726 4.80875ZM13.0014 6.3515C12.2521 6.33981 11.508 6.47729 10.8123 6.75594C10.1167 7.0346 9.48346 7.44885 8.94946 7.97458C8.41546 8.50032 7.99138 9.12703 7.70191 9.81823C7.41244 10.5094 7.26336 11.2513 7.26336 12.0007C7.26336 12.7501 7.41244 13.4919 7.70191 14.1831C7.99138 14.8743 8.41546 15.5011 8.94946 16.0268C9.48346 16.5525 10.1167 16.9668 10.8123 17.2454C11.508 17.5241 12.2521 17.6616 13.0014 17.6499C14.4844 17.6267 15.8988 17.0214 16.9393 15.9645C17.9799 14.9076 18.5631 13.4839 18.5631 12.0007C18.5631 10.5175 17.9799 9.09382 16.9393 8.03691C15.8988 6.97999 14.4844 6.37464 13.0014 6.3515ZM13.0014 8.33288C13.974 8.33288 14.9067 8.71923 15.5944 9.40695C16.2821 10.0947 16.6685 11.0274 16.6685 12C16.6685 12.9726 16.2821 13.9053 15.5944 14.593C14.9067 15.2808 13.974 15.6671 13.0014 15.6671C12.0288 15.6671 11.096 15.2808 10.4083 14.593C9.72061 13.9053 9.33425 12.9726 9.33425 12C9.33425 11.0274 9.72061 10.0947 10.4083 9.40695C11.096 8.71923 12.0288 8.33288 13.0014 8.33288Z"
                     fill="#23A6F0"
@@ -427,7 +574,7 @@ export default function Home() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <g clip-path="url(#clip0_2984_7851)">
+                <g clipPath="url(#clip0_2984_7851)">
                   <path
                     d="M8.91075 19C17.213 19 21.7546 12.0731 21.7546 6.07669C21.7546 5.88285 21.7546 5.68625 21.7464 5.49241C22.6306 4.84789 23.3937 4.04993 24 3.13592C23.1741 3.50262 22.2988 3.7443 21.4026 3.85312C22.3466 3.28495 23.0536 2.391 23.3923 1.3374C22.5054 1.86647 21.5346 2.23787 20.5226 2.43534C19.8423 1.70572 18.9419 1.22237 17.9611 1.06016C16.9803 0.897946 15.9737 1.06592 15.0973 1.53807C14.2209 2.01023 13.5236 2.7602 13.1134 3.67182C12.7031 4.58345 12.6029 5.60585 12.8281 6.58066C11.0334 6.49005 9.27762 6.02057 7.67468 5.20269C6.07175 4.3848 4.65744 3.23678 3.5235 1.83307C2.94784 2.83421 2.77216 4.01843 3.03214 5.14526C3.29211 6.27209 3.96825 7.25705 4.92325 7.90013C4.20762 7.87565 3.50772 7.68215 2.88 7.33524V7.39754C2.88123 8.44631 3.24205 9.46247 3.90145 10.2743C4.56086 11.0861 5.4784 11.6438 6.499 11.853C6.11161 11.9605 5.71144 12.014 5.30963 12.0122C5.02635 12.0131 4.74365 11.9867 4.46537 11.9333C4.75383 12.8361 5.3155 13.6254 6.07171 14.1908C6.82793 14.7561 7.74081 15.0691 8.6825 15.0859C7.08276 16.3511 5.10668 17.0373 3.0725 17.034C2.71407 17.0355 2.35588 17.0147 2 16.9716C4.06458 18.297 6.46228 19.0008 8.91075 19Z"
                     fill="#23A6F0"
@@ -450,7 +597,7 @@ export default function Home() {
 
         <hr className=" max-w-[1050px]  mx-auto text-[#E6E6E6] mb-12" />
 
-        <div className=" max-w-[1050px] h-[136px] mx-auto flex justify-between ]">
+        <div className=" font-primary max-w-[1050px] h-[136px] mx-auto flex justify-between ]">
           <div className="space-y-5">
             <h3 className="text-[23px] text-[#252B42] font-bold">
               Company Info
@@ -512,7 +659,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="bg-[#FAFAFA]">
+        <div className="bg-[#FAFAFA] font-primary">
           <div className=" h-[74px] py-6 max-w-[1050px]  mx-auto text-[#737373] text-[18px] mt-[70px]">
             Made With Love By Finland All Right Reserved
           </div>
